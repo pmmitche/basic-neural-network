b9a320778599776de958efdc06eaa0cb171bf54b
Parker Mitchell <parker.mitchell1@gmail.com>
2021-05-12 21:08:16 -0700

    Created Neuron and NeuralNetwork classes

---
+++ NeuralNetwork.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+def sigmoid(x):
+    # f(x) = 1 / (1 + e^(-x))
+    return 1 / (1 + np.exp(-x))
+
+class Neuron:
+    def __init__(self, weights, bias):
+        self.weights = weights
+        self.bias = bias
+
+    def feedforward(self, inputs):
+        result = np.dot(self.weights, inputs) + self.bias
+        return sigmoid(result)
+
+
+class NeuralNetwork:
+    '''
+    Class for a neural network with:
+        - 2 inputs
+        - one hidden layer with 2 neurons (h1, h2)
+        - one output layer with 1 neuron (o1)
+    Each neuron has weights w = [0, 1], and bias b = 0
+    '''
+    def __init__(self):
+        weights = np.array([0, 1])
+        bias = 0
+
+        self.h1 = Neuron(weights, bias)
+        self.h2 = Neuron(weights, bias)
+        self.o1 = Neuron(weights, bias)
+
+    def feedforward(self, inputs):
+        h1_out = self.h1.feedforward(inputs)
+        h2_out = self.h2.feedforward(inputs)
+
+        out_o1_input = np.array([h1_out, h2_out])
+        o1_out = self.o1.feedforward(out_o1_input)
+        
+        return o1_out
+
+    
+    
